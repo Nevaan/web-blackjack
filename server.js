ecad988c00f601a54dc3ff899fc67035887bc80c
Pawel <pawel.losek@outlook.com>
2017-03-19 17:53:24 +0100

    Added proper path to compiles ts files for heroku

--- server.js
+++ server.js
@@ -5,14 +5,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var appPath = __dirname + '/build';
 
-app.use(express.static(__dirname));
-app.use("/styles", express.static(__dirname + '/styles'));
-app.use("/images", express.static(__dirname + '/assets/images'));
-app.use("/scripts", express.static(__dirname + '/js'));
+app.use(express.static(appPath));
+app.use("/styles", express.static(appPath + '/styles'));
+app.use("/images", express.static(appPath + '/assets/images'));
+app.use("/scripts", express.static(appPath + '/js'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+ '/index.html'));
+    res.sendFile(path.join(appPath+ '/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
