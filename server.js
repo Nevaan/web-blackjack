fe6ec9d9bc80c962ccb4643ef53fc6ba2f3e74db
Pawel <pawel.losek@outlook.com>
2017-03-19 15:21:18 +0100

    Added configuration for heroku deploy

--- server.js
+++ server.js
@@ -5,17 +5,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var filePath = 'build/';
+var appPath = __dirname + '/build/';
 
-
-app.use(express.static(filePath));
-app.use("/styles", express.static(filePath + 'styles'));
-app.use("/images", express.static(filePath + 'assets/images'));
-app.use("/scripts", express.static(filePath + 'js'));
+app.use(express.static(appPath));
+app.use("/styles", express.static(appPath + 'styles'));
+app.use("/images", express.static(appPath + 'assets/images'));
+app.use("/scripts", express.static(appPath + 'js'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-    res.sendFile(path.join(filePath+ 'index.html'));
+    res.sendFile(path.join(appPath+ 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
